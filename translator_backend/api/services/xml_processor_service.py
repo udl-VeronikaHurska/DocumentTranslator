e4852ef5216ff7fdc134c606f0f0974a770b7b9e
VeronikaHurska <nika.gurska@gmail.com>
2024-08-05 21:10:28 +0300

    fixed xml layout solution

--- translator_backend/api/services/xml_processor_service.py
+++ translator_backend/api/services/xml_processor_service.py
@@ -27,21 +27,16 @@ class XMLProcessor:
                 elem.text = translated_sentences[sentence_index]
                 sentence_index += 1
 
-        return etree.tostring(root, pretty_print=True, encoding='unicode')
+        return etree.tostring(root, pretty_print=True, encoding='utf-8')
 
     def convert_to_docx(self, xml_content):
-        document = Document()
-        document.add_paragraph(xml_content)
         file_stream = BytesIO()
-        document.save(file_stream)
-        file_stream.seek(0)
-        return file_stream
-
-    def convert_to_doc(self, xml_content):
-        file_stream = BytesIO()
-        with olefile.OleFileIO() as ole:
-            ole.write_stream('WordDocument', xml_content.encode('utf-8'))
-            ole.save(file_stream)
+        with zipfile.ZipFile(file_stream, 'w') as z:
+            z.writestr('word/document.xml', xml_content)
+            # Add all necessary DOCX parts here (content_types, rels, etc.)
+            z.writestr('[Content_Types].xml', self.get_content_types())
+            z.writestr('_rels/.rels', self.get_rels())
+            z.writestr('word/_rels/document.xml.rels', self.get_word_rels())
         file_stream.seek(0)
         return file_stream
 
@@ -58,9 +53,6 @@ class XMLProcessor:
         if output_format == 'docx':
             file_stream = self.convert_to_docx(translated_xml)
             media_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        elif output_format == 'doc':
-            file_stream = self.convert_to_doc(translated_xml)
-            media_type = 'application/msword'
         else:
             raise HTTPException(status_code=400, detail="Unsupported output format")
 
@@ -73,11 +65,21 @@ class XMLProcessor:
             xml_content = z.read('word/document.xml')
         return xml_content
 
-    def extract_xml_from_doc(self, file):
-        ole = olefile.OleFileIO(file)
-        if ole.exists('WordDocument'):
-            stream = ole.openstream('WordDocument')
-            doc_content = stream.read()
-            return f"<xml><content>{doc_content}</content></xml>"
-        else:
-            raise HTTPException(status_code=400, detail="Unsupported DOC file format")
+    def get_content_types(self):
+        return '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+    <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+    <Default Extension="xml" ContentType="application/xml"/>
+    <Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/>
+</Types>'''
+
+    def get_rels(self):
+        return '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+    <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/>
+</Relationships>'''
+
+    def get_word_rels(self):
+        return '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+</Relationships>'''
